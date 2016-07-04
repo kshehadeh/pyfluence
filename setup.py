1a599ac0656ed9cdf41f6367223ff9645dcaae39
Karim Shehadeh <kshehadeh@underarmour.com>
2016-07-04 09:08:04 -0400

    cleanup dist and add test files

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='Confluence REST API client for Python',
     long_description=long_description,
@@ -87,7 +87,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        # 'sample': ['package_data.dat'],
+        # 'readme': ['README.md'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
